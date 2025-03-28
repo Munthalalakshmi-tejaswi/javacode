ab018d7b77ebde636851ac50509f87f45f1fc256
munthalalakshmitejaswi <22102A040176@mbu.asia>
2025-03-28 22:16:27 +0530

    javacodesmany

---
+++ Sequencesqusum.java
@@ -0,0 +1,9 @@
+class Sequencesqusum{
+    public static void main(String[] args){
+        int n=5,sum=0;
+        for(int i=1;i<=n;i++){
+            sum=sum+i*i;
+        }
+        System.out.println(sum);
+    }
+}
