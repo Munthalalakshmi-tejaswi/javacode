72a3cda29746b93fe6a6fde1cd96a155b6875a58
munthalalakshmitejaswi <22102A040176@mbu.asia>
2025-04-24 22:03:15 +0530

    javacode

---
+++ Filedelete.java
@@ -0,0 +1,15 @@
+import java.io.File;
+
+class Filedelete {
+  public static void main(String[] args) {
+
+    File file = new File("JavaFile.java");
+    boolean value = file.delete();
+    if(value) {
+      System.out.println("JavaFile.java is successfully deleted.");
+    }
+    else {
+      System.out.println("File doesn't exit");
+    }
+  }
+}
